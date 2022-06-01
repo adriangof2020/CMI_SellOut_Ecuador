898a4767e03216b34d2244487163f9ce89197109
adrian <adrian1820@hotmail.com>
2022-06-01 07:11:13 -0500

    agregue comentarios

--- Validador.sql
+++ Validador.sql
@@ -1,4 +1,6 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
+--Tablero
+
 SELECT *
   FROM [CmiSellOutEcuador].[dbo].[VENTAS_TABLERO] WHERE Territorio IS NULL
 
@@ -10,4 +12,16 @@ FROM [CmiSellOutEcuador].[dbo].[VENTAS_TABLERO] WHERE CodAlicorp IS NULL
 
 SELECT *
 FROM [CmiSellOutEcuador].[dbo].[VENTAS_TABLERO] WHERE Mes IS NULL
+
+--VENTAS_CONSOLIDADO
+
+--SELECT *
+--  FROM [CmiSellOutEcuador].[dbo].VENTAS_CONSOLIDADO WHERE Territorio IS NULL
+
+--SELECT *
+--FROM [CmiSellOutEcuador].[dbo].VENTAS_CONSOLIDADO WHERE Familia IS NULL
+
+
+--SELECT *
+--FROM [CmiSellOutEcuador].[dbo].VENTAS_CONSOLIDADO WHERE Mes IS NULL
 
