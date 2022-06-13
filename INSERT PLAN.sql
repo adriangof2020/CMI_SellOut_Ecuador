d878a813cbf5ccfce45b22f22b73c3c8261a7f73
adrian <adrian1820@hotmail.com>
2022-06-13 11:36:47 -0500

    update

--- INSERT PLAN.sql
+++ INSERT PLAN.sql
@@ -9,25 +9,31 @@ SELECT A.CodAlicorp, B.CodAlicorp C
 SELECT A.CodAlicorp, B.CodAlicorp C
   FROM PLAN_PANALES A LEFT JOIN MAESTRO_ALICORP B ON A.CodAlicorp = B.CodAlicorp WHERE B.CodAlicorp IS NULL
 
-IF OBJECT_ID(N'tempdb..#PLANES') IS NOT NULL DROP TABLE #PLANES;
-SELECT *
-INTO #PLANES
-FROM PLAN_2MAYA
+--IF OBJECT_ID(N'tempdb..#PLANES') IS NOT NULL DROP TABLE #PLANES;
+--SELECT *
+--INTO #PLANES
+--FROM PLAN_2MAYA
 
-INSERT INTO #PLANES
-SELECT *
-FROM PLAN_PANALES
+--INSERT INTO #PLANES
+--SELECT *
+--FROM PLAN_PANALES
 
-INSERT INTO #PLANES
-SELECT *
-FROM PLAN_HULARUSS
+--INSERT INTO #PLANES
+--SELECT *
+--FROM PLAN_HULARUSS
  
 INSERT INTO MAESTRO_ALICORP
 SELECT C.*, D.PesoKG, (D.PesoKG/1000) PesoTon, D.FacUnitario
 FROM 
-(SELECT	  DISTINCT B.CodAlicorp, B.Des_Material Material, B.CodCategoria, B.Categoria,
+--(SELECT	  DISTINCT B.CodAlicorp, B.Des_Material Material, B.CodCategoria, B.Categoria,
+					--B.CodFamilia, B.Familia, B.CodMarca, B.Marca, B.Plataforma
+					(SELECT DISTINCT B.CodAlicorp, B.Des_Material Material, B.CodCategoria, B.Categoria,
 					B.CodFamilia, B.Familia, B.CodMarca, B.Marca, B.Plataforma
-FROM    #PLANES B 
+					FROM (SELECT * FROM PLAN_HULARUSS 
+					UNION ALL SELECT * FROM PLAN_PANALES
+					UNION ALL SELECT *  FROM PLAN_2MAYA) B
+
+--FROM    #PLANES B 
 WHERE  B.CodAlicorp NOT IN (SELECT DISTINCT CodAlicorp FROM MAESTRO_ALICORP)) C
 LEFT JOIN MAESTRO_FERHAWA D ON C.CodAlicorp = D.CodAlicorp
 
