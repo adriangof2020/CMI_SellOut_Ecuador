2a22a7c0cd1b3dce8ff79900c3a99a98334554c0
adrian <adrian1820@hotmail.com>
2022-07-14 20:55:52 -0500

    update

---
+++ gabriela.sql
@@ -0,0 +1,31 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [Mes]
+      ,[Dia]
+      ,[CodCategoria]
+      ,[Categoria]
+      ,[CodFamilia]
+      ,[Familia]
+      ,[CodAlicorp]
+      ,[Material]
+      ,[CodMarca]
+      ,[Marca]
+      
+      ,[Oficina_Ventas]
+      ,[Grupo_Vendedores]
+     
+      ,[Agrupacion_Distribuidora]
+      ,[Agencia_Distribuidora]
+    
+      ,[Grupo_Condiciones]
+     
+      ,[CodClienteSellOut]
+      ,[ClienteSellOut]
+     
+    
+      
+      ,[Plan_Ton]
+      ,[real_ton]
+     
+      ,[Plataforma]
+  FROM [CmiSellOutEcuador].[dbo].[VENTAS_TABLERO]
+
