c7994630a89b20c74acc18823c4d0cf4d9f59da7
adrian <adrian1820@hotmail.com>
2022-08-02 08:05:35 -0500

    update

--- gabriela.sql
+++ gabriela.sql
@@ -1,26 +1,10 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-SELECT [Mes]
-      ,[Dia]
-      ,[CodCategoria]
-      ,[Categoria]
-      ,[CodFamilia]
-      ,[Familia]
-      ,[CodAlicorp]
-      ,[Material]
-      ,[CodMarca]
-      ,[Marca]
-      ,[Oficina_Ventas]
-      ,[Grupo_Vendedores]
-      ,[Agrupacion_Distribuidora]
-      ,[Agencia_Distribuidora]
-      ,[Grupo_Condiciones]
-       ,[CodClienteSellOut]
-      ,[ClienteSellOut]
-	  ,Plan_Dol
-	  ,real_Dolares
-      ,[Plan_Ton]
-      ,[real_ton]
-      ,[Plataforma]
-  FROM [CmiSellOutEcuador].[dbo].[VENTAS_TABLERO]
+--Lanzamiento
+SELECT Mes, Dia,
+	   CodCategoria, Categoria, CodFamilia, Familia, CodAlicorp, Material, CodMarca, Marca,
+	   Oficina_Ventas, Grupo_Vendedores, Agrupacion_Distribuidora, Agencia_Distribuidora, Grupo_Condiciones,
+       CodClienteSellOut, ClienteSellOut,
+	   Plan_Dol, real_Dolares, Plan_Ton, real_ton, Plataforma
+  FROM CmiSellOutEcuador.dbo.VENTAS_TABLERO
   WHERE Dia LIKE '%07/2022' AND CodClienteSellOut <> 'Dummy'
 
