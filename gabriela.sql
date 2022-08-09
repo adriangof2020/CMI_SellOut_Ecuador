608b8c37f67a69071d86ff66c0d153909a122f13
adrian <adrian1820@hotmail.com>
2022-08-09 16:01:29 -0500

    update

--- gabriela.sql
+++ gabriela.sql
@@ -3,8 +3,8 @@
 SELECT Mes, Dia,
 	   CodCategoria, Categoria, CodFamilia, Familia, CodAlicorp, Material, CodMarca, Marca,
 	   Oficina_Ventas, Grupo_Vendedores, Agrupacion_Distribuidora, Agencia_Distribuidora, Grupo_Condiciones,
-       CodClienteSellOut, ClienteSellOut,
+       CodClienteSellOut, ClienteSellOut, Vendedor_Distribuidora,
 	   Plan_Dol, real_Dolares, Plan_Ton, real_ton, Plataforma
   FROM CmiSellOutEcuador.dbo.VENTAS_TABLERO
-  WHERE Dia LIKE '%07/2022' AND CodClienteSellOut <> 'Dummy'
+  WHERE Dia LIKE '%08/2022' AND CodClienteSellOut <> 'Dummy'
 
