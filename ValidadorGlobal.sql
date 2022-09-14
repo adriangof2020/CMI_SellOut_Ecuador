5a135ffd777d8746245ef5dba946f687a8e256c3
adrian <adrian1820@hotmail.com>
2022-09-14 09:28:26 -0500

    u

---
+++ ValidadorGlobal.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  TipoDistribuidor ,Agrupacion_Distribuidora,  SUM(Real_Ton) toneladas FROM STAYING_SO_ECUADOR
+WHERE Mes = 'Set-22'
+GROUP BY TipoDistribuidor ,Agrupacion_Distribuidora
+ORDER BY 1
+
+
+SELECT  TipoDistribuidor ,Agrupacion_Distribuidora,  SUM(Real_Ton) toneladas FROM [DATA_MART_SO_ECUADOR]
+WHERE Mes = 'Set-22'
+GROUP BY TipoDistribuidor ,Agrupacion_Distribuidora
+ORDER BY 1
