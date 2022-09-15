55bf9a0c9f355d6375aa5f06c5c7600a9c1a7575
adrian <adrian1820@hotmail.com>
2022-09-15 12:13:12 -0500

    u

--- ReporteMacrodistribuidores.sql
+++ ReporteMacrodistribuidores.sql
@@ -4,5 +4,5 @@ SELECT @ultimoDiaVentas = DATEADD(DAY,-1,SYSDATETIME());
 
 EXEC sp_ReporteMacrodistribuidoresSOEcuador
 	 @dia = @ultimoDiaVentas
-	,@MesAnterior = 'Set-21'
-	,@MesAñoAnterior = 'Ago-22'
+	,@MesAnterior = 'Ago-22'
+	,@MesAñoAnterior = 'Set-21'
