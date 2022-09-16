26749d2010059e578b8de1ed5424d83a9681911c
adrian <adrian1820@hotmail.com>
2022-09-15 21:16:24 -0500

    u

--- ReporteSOEcuador.sql
+++ ReporteSOEcuador.sql
@@ -2,7 +2,8 @@ DECLARE @ultimoDiaVentas DATE;
 SELECT @ultimoDiaVentas = DATEADD(DAY,-1,SYSDATETIME());
 
 
-EXEC sp_ReporteMacrodistribuidoresSOEcuador
+EXEC sp_ReporteSOEcuador
 	 @dia = @ultimoDiaVentas
 	,@MesAnterior = 'Ago-22'
 	,@MesAñoAnterior = 'Set-21'
+	,@TipoDistribuidor = 'MINORISTAS'
